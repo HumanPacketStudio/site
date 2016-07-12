8c63715923d134bdd5e99e98d23a607a404d84c9
Lalera123 <magadeevavaleri@gmail.com>
2016-07-12 19:12:48 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -12,53 +12,43 @@
 
 <body>
 
-  <div id="_1">
+  <div id="_1"  >
 
-   <a href="index.html"> <img src="images/logo.png"  id="logo"> </a>
-      <a href="abiturient.html"><img src="images/%D0%B0%D0%B1%D0%B8%D1%82%D1%83%D1%80%D0%B8%D0%B5%D0%BD%D1%82%D1%83.png" id="btn1" ></a>
-      <a href="student.html"> <img src="images/%D1%81%D1%82%D1%83%D0%B4%D0%B5%D0%BD%D1%82%D1%83.png" id="btn2"></a>
-      <a href="prepod.html"> <img src="images/%D0%BF%D1%80%D0%B5%D0%BF%D0%BE%D0%B4%D0%B0%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8E.png" id="btn3"></a>
+   <div id="logo">
+   <a href="index.php"> <img src="images/logo.png"> </a>
+      </div>
+   <div id="butt">
+      <a href="abiturient.php"><img src="images/%D0%B0%D0%B1%D0%B8%D1%82%D1%83%D1%80%D0%B8%D0%B5%D0%BD%D1%82%D1%83.png" id="btn1" ></a>
+      <a href="student.php"> <img src="images/%D1%81%D1%82%D1%83%D0%B4%D0%B5%D0%BD%D1%82%D1%83.png" id="btn2"></a>
+      <a href="prepod.php"> <img src="images/%D0%BF%D1%80%D0%B5%D0%BF%D0%BE%D0%B4%D0%B0%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8E.png" id="btn3"></a>
+    </div>
 
-    
     <div id="menu">
 
-      <div id="m1" class="nm-label">
-        <a id="m1_1" href="mp.html">
-            Мероприятия<br/>
-        </a>
-      </div>
-
-      <div id="m2" class="nm-label">
-        <a id="_m2_2" href="vectors.html">
-            Направления<br/>
-        </a>
-      </div>
-
-      <div id="m3" class="nm-label">
-        <a id="m3_3" href="science.html">
-            Наука<br/>
-        </a>
-      </div>
-
-      <div id="m4" class="nm-label">
-        <a id="m4_4" href="sotrudniki.html">
-            Сотрудники<br/>
-        </a>
-      </div>
-
-      <div id="m5" class="nm-label">
-        <a id="m5_5" href="contacts.html">
-            Контакты<br/>
-        </a>
-        
-        <div id="m6" class="nm-label">
-        <a id="m6_6" href="about.html">
-            О кафедре<br/>
-        </a>
-      </div>
+        <a class="buttonk" href="about.php">
+             </a><br/>
+     
+        <a class="buttonN" href="vectors.php">
+            </a> <br/>
       
-      </div>
+        <a class="buttonS" href="science.php">
+            </a> <br/>
+      
+        <a class="buttonSo" href="sotrudniki.php">
+        </a> <br/>
+    
+        <a class="buttonKo" href="contacts.php"> 
+        </a><br/>
+		
+        <a class="buttonMp" href="mp.php">  
+        </a> <br/>
+      
+      
+      
     </div>
+	<?php
+	require_once('vectors.php');
+	?>
   </div>
   <script type="text/javascript" src="../common-files/js/require.min.js" data-main="../scripts/startup"></script>
 </body>
